1ea12c49a5d92c85d1a4f53338cb55367fef6011
kiles-maker <152271840+kiles-maker@users.noreply.github.com>
2023-11-28 22:47:45 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Yes)
+    basic.showIcon(IconNames.No)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Square)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.StickFigure)
+})
